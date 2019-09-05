a4aeea180fcf6903184be4d2bc8cc3345450fe47
sy <1404341518@qq.com>
2019-09-05 17:29:21 +0800

    who

--- app/controllers/DataController.php
+++ app/controllers/DataController.php
@@ -20,7 +20,7 @@ class DataController extends ControllerBase
 
         $cmd = "scp -r abin@$server_url:$route /usr/local/redis;./redis-cli -h 127.0.0.1 -p 6379 --pipe < appendonly.aof;rm appendonly.aof  && echo success";
         //$cmd = 'cd /usr/local/redis;mkdir test && echo success';
-        //$cmd = 'whoami';
+        $cmd = 'whoami';
         $result = shell_exec($cmd);
         if($result){
             $this->functions->alert('更新成功');
