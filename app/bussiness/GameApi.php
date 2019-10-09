5612438f2a59155db14ddc875ca69664e95048e5
sy <1404341518@qq.com>
2019-10-09 17:56:27 +0800

    test

--- app/bussiness/GameApi.php
+++ app/bussiness/GameApi.php
@@ -153,7 +153,7 @@ class GameApi extends BaseBussiness
         }
 
         if(!isset($result['success']) || $result['success'] != 'true'){
-            var_dump($result);exit;
+//            var_dump($result);exit;
             return false;
         }
         return true;
