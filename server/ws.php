6daf2568ad0012a67237ceb0924912aaa5debf44
sy <1404341518@qq.com>
2020-01-15 18:14:08 +0800

    swoole

--- server/ws.php
+++ server/ws.php
@@ -122,7 +122,7 @@ class Ws{
     //监听ws连接事件
     public function onOpen($ws, $request){
 
-        $admin = $request->session;
+        $admin = $request->redis;
         var_dump($admin);
         //将fd放入redis有序集合
         var_dump('fd='.$request->fd);
